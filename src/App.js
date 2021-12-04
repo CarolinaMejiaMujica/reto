9d4e16cda006d85c3afd96ffb97bea3da5a9b175
CarolinaMejiaMujica <carolinae.mejiam@pucp.edu.pe>
2021-12-04 18:28:03 -0500

    actualización online

--- src/App.js
+++ src/App.js
@@ -1,337 +1,103 @@
 import "./App.css";
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import FadeLoader from "react-spinners/FadeLoader";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TablePagination from "@material-ui/core/TablePagination";
-import TableRow from "@material-ui/core/TableRow";
-import PropTypes from "prop-types";
-import { makeStyles, useTheme } from "@material-ui/core/styles";
-import IconButton from "@material-ui/core/IconButton";
-import FirstPageIcon from "@material-ui/icons/FirstPage";
-import KeyboardArrowLeft from "@material-ui/icons/KeyboardArrowLeft";
-import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
-import LastPageIcon from "@material-ui/icons/LastPage";
-import { Box, Grid, Typography } from "@material-ui/core";
-import Axios from "axios";
-const columns = [
-  { id: "nombre", label: "Departamento", minWidth: 170 },
-  {
-    id: "codigo",
-    label: "ID de acceso de la secuencia genómica \u00a0(*)",
-    minWidth: 100,
-  },
-  {
-    id: "fecha",
-    label: "Fecha de recolección",
-    minWidth: 170,
-    align: "center",
-    fontWeight: 500,
-    background: "#FFFFFF",
-    format: (value) => value.toLocaleString("en-US"),
-  },
-  {
-    id: "nomenclatura",
-    label: "Nomenclatura según la OMS de la variante identificada",
-    minWidth: 170,
-    align: "center",
-    background: "#FFFFFF",
-    format: (value) => value.toLocaleString("en-US"),
-  },
-  {
-    id: "variante",
-    label: "Nombre de la variante identificada",
-    minWidth: 170,
-    align: "center",
-    background: "#FFFFFF",
-    format: (value) => value.toFixed(2),
-  },
-];
+import Checkbox from "@mui/material/Checkbox";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
+import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
+import CheckBoxIcon from "@mui/icons-material/CheckBox";
+import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles({
-  spinner: {
-    padding: "10px",
-    marginTop: "200px",
-    align: "center",
-    textAlign: "center",
-  },
-  root: {
-    width: "100%",
-  },
-  container: {
-    maxHeight: 440,
-  },
-  paper2: {
-    backgroundColor: "#ffffff",
+const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
+const checkedIcon = <CheckBoxIcon fontSize="small" />;
+
+const departamentos = [
+  "Todos",
+  "Amazonas",
+  "Áncash",
+  "Apurímac",
+  "Arequipa",
+  "Ayacucho",
+  "Cajamarca",
+  "Callao",
+  "Cusco",
+  "Huancavelica",
+  "Huánuco",
+  "Ica",
+  "Junín",
+  "La Libertad",
+  "Lambayeque",
+  "Lima",
+  "Loreto",
+  "Madre de Dios",
+  "Moquegua",
+  "Pasco",
+  "Piura",
+  "Puno",
+  "San Martín",
+  "Tacna",
+  "Tumbes",
+  "Ucayali",
+];
+const useStyles = makeStyles((theme) => ({
+  title: {
+    display: "flex",
+    flexDirection: "center",
+    fontWeight: 600,
     padding: "10px",
     borderRadius: "5px",
     margin: "10px",
+    justifyContent: "center",
+    alignItems: "center",
   },
-  bold: {
-    fontWeight: 600,
-  },
-  imagen: {
-    marginLeft: "10px",
-    color: "#0000",
-  },
-  pagination: {
-    paddingLeft: "800px",
-  },
-});
-
-const useStyles1 = makeStyles((theme) => ({
-  root: {
-    flexShrink: 0,
-    marginLeft: theme.spacing(2.5),
+  image: {
+    paddingTop: "20px",
+    width: "95%",
   },
 }));
-
-function TablePaginationActions(props) {
-  const classes = useStyles1();
-  const theme = useTheme();
-  const { count, page, rowsPerPage, onPageChange } = props;
-
-  const handleFirstPageButtonClick = (event) => {
-    onPageChange(event, 0);
-  };
-
-  const handleBackButtonClick = (event) => {
-    onPageChange(event, page - 1);
-  };
-
-  const handleNextButtonClick = (event) => {
-    onPageChange(event, page + 1);
-  };
-
-  const handleLastPageButtonClick = (event) => {
-    onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
-  };
+function App() {
+  const classes = useStyles();
+  const [seleccionados, setSeleccionados] = React.useState(departamentos);
 
   return (
-    <div className={classes.root}>
-      <IconButton
-        onClick={handleFirstPageButtonClick}
-        disabled={page === 0}
-        aria-label="Primera página"
-      >
-        {theme.direction === "rtl" ? <LastPageIcon /> : <FirstPageIcon />}
-      </IconButton>
-      <IconButton
-        onClick={handleBackButtonClick}
-        disabled={page === 0}
-        aria-label="previous page"
-      >
-        {theme.direction === "rtl" ? (
-          <KeyboardArrowRight />
-        ) : (
-          <KeyboardArrowLeft />
+    <div>
+      <div className={classes.title}>Reto Semana 4 - IBM</div>
+      <Autocomplete
+        value={seleccionados}
+        id="departamentos"
+        multiple
+        options={departamentos}
+        disableCloseOnSelect
+        getOptionLabel={(option) => option}
+        onChange={(event, newValue) => {
+          if (newValue.includes("Todos")) {
+            setSeleccionados(departamentos);
+          } else if (newValue.length === 0) {
+            setSeleccionados([]);
+          } else {
+            setSeleccionados(newValue);
+          }
+        }}
+        renderOption={(props, option, { selected }) => (
+          <li {...props}>
+            <Checkbox
+              icon={icon}
+              checkedIcon={checkedIcon}
+              style={{ marginRight: 8 }}
+              checked={selected}
+            />
+            {option}
+          </li>
         )}
-      </IconButton>
-      <IconButton
-        onClick={handleNextButtonClick}
-        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-        aria-label="Siguiente página"
-      >
-        {theme.direction === "rtl" ? (
-          <KeyboardArrowLeft />
-        ) : (
-          <KeyboardArrowRight />
+        style={{ width: "90%", paddingLeft: 20, paddingRight: 20 }}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label="Departamentos"
+            placeholder="Departamento"
+          />
         )}
-      </IconButton>
-      <IconButton
-        onClick={handleLastPageButtonClick}
-        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-        aria-label="Última página"
-      >
-        {theme.direction === "rtl" ? <FirstPageIcon /> : <LastPageIcon />}
-      </IconButton>
+      />
     </div>
   );
 }
-
-TablePaginationActions.propTypes = {
-  count: PropTypes.number.isRequired,
-  onPageChange: PropTypes.func.isRequired,
-  page: PropTypes.number.isRequired,
-  rowsPerPage: PropTypes.number.isRequired,
-};
-function App() {
-  const classes = useStyles();
-  const departamentos = [
-    "Todos",
-    "Amazonas",
-    "Áncash",
-    "Apurímac",
-    "Arequipa",
-    "Ayacucho",
-    "Cajamarca",
-    "Callao",
-    "Cusco",
-    "Huancavelica",
-    "Huánuco",
-    "Ica",
-    "Junín",
-    "La Libertad",
-    "Lambayeque",
-    "Lima",
-    "Loreto",
-    "Madre de Dios",
-    "Moquegua",
-    "Pasco",
-    "Piura",
-    "Puno",
-    "San Martín",
-    "Tacna",
-    "Tumbes",
-    "Ucayali",
-  ];
-  const inicioDate = "Mon Apr 06 2020 20:51:01 GMT-0500";
-  const finDate = "Fri Oct 15 2021 20:00:01 GMT-0500";
-
-  function convert(str) {
-    var date = new Date(str);
-    var mnth = ("0" + (date.getMonth() + 1)).slice(-2);
-    var day = ("0" + date.getDate()).slice(-2);
-    return [date.getFullYear(), mnth, day].join("-");
-  }
-  const fechaIni = convert(inicioDate);
-  const fechaFin = convert(finDate);
-  const deps = departamentos;
-  const params = `fechaIni=${fechaIni}&fechaFin=${fechaFin}`;
-  const [filas, setFilas] = React.useState([]);
-
-  const [cargando, setCargando] = React.useState(true);
-  const [bandera, setBandera] = React.useState(false);
-
-  React.useEffect(() => {
-    Axios.post(`http://3.86.154.241/tablaespacio/?${params}`, deps)
-      .then((response) => {
-        const val1 = response.data;
-        if (val1 === "No hay datos") {
-          setBandera(true);
-        } else {
-          setBandera(false);
-          setFilas(response.data);
-          setCargando(false);
-        }
-      })
-      .catch((err) => console.log(err));
-    // eslint-disable-next-line
-  }, []);
-
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
-
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
-  };
-
-  return (
-    <Grid item xs={12} sm={12}>
-      {!bandera && (
-        <Box className={classes.paper2} boxShadow={0} height={580}>
-          <Grid container justifyContent="space-between">
-            <Typography variant="h6" align="left" className={classes.bold}>
-              Datos de las secuencias genómicas SARS-CoV-2
-            </Typography>
-          </Grid>
-          {cargando && (
-            <div className={classes.spinner}>
-              <FadeLoader
-                className={classes.spinner}
-                color="#003E97"
-                radius={20}
-                height={15}
-                width={5}
-                margin="2px"
-              />
-            </div>
-          )}
-          {!cargando && (
-            <TableContainer className={classes.container}>
-              <Table stickyHeader>
-                <TableHead>
-                  <TableRow>
-                    {columns.map((column) => (
-                      <TableCell
-                        key={column.id}
-                        align="center"
-                        background={column.background}
-                        style={{
-                          minWidth: column.minWidth,
-                          fontWeight: column.fontWeight,
-                        }}
-                      >
-                        {column.label}
-                      </TableCell>
-                    ))}
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {filas
-                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                    .map((row) => {
-                      return (
-                        <TableRow
-                          hover
-                          role="checkbox"
-                          aria-checked="false"
-                          tabIndex={-1}
-                          key={row.codigo}
-                        >
-                          {columns.map((column) => {
-                            const value = row[column.id];
-                            return (
-                              <TableCell key={column.id} align="center">
-                                {column.format && typeof value === "number"
-                                  ? column.format(value)
-                                  : value}
-                              </TableCell>
-                            );
-                          })}
-                        </TableRow>
-                      );
-                    })}
-                </TableBody>
-              </Table>
-            </TableContainer>
-          )}
-          {!cargando && (
-            <Grid container justifyContent="space-between">
-              <div style={{ paddingTop: "15px" }}>
-                (*) Identificador en la base de datos GISAID.
-              </div>
-              <TablePagination
-                className="mx-auto"
-                labelRowsPerPage={"Filas por página"}
-                rowsPerPageOptions={[10, 15, 20]}
-                component="div"
-                count={filas.length}
-                labelDisplayedRows={({ from, to, count }) => {
-                  return "" + from + "-" + to + " de " + count;
-                }}
-                rowsPerPage={rowsPerPage}
-                page={page}
-                onPageChange={handleChangePage}
-                onRowsPerPageChange={handleChangeRowsPerPage}
-                ActionsComponent={TablePaginationActions}
-              ></TablePagination>
-            </Grid>
-          )}
-        </Box>
-      )}
-      {bandera && <Box></Box>}
-    </Grid>
-  );
-}
-
 export default App;
